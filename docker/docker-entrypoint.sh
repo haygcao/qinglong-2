aab4ab881c49fd30d9c9dca16ae92a86253d5847
ferryvip <ferryvip@163.com>
2021-06-18 19:55:56 +0800

    更新仓库代码

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -10,7 +10,8 @@ cp -fv $dir_root/docker/front.conf /etc/nginx/conf.d/front.conf
 echo
 
 echo -e "======================2. 更新源代码========================\n"
-ql update
+ql update "no-restart"
+pm2 l >/dev/null 2>&1
 echo
 
 echo -e "======================3. 启动nginx========================\n"
@@ -18,7 +19,6 @@ nginx -s reload 2>/dev/null || nginx -c /etc/nginx/nginx.conf
 echo -e "nginx启动成功...\n"
 
 echo -e "======================4. 启动控制面板========================\n"
-cd $dir_root
 if [[ $(pm2 info panel 2>/dev/null) ]]; then
   pm2 reload panel
 else
@@ -27,7 +27,6 @@ fi
 echo -e "控制面板启动成功...\n"
 
 echo -e "======================5. 启动定时任务========================\n"
-cd $dir_root
 if [[ $(pm2 info schedule 2>/dev/null) ]]; then
   pm2 reload schedule
 else
@@ -37,7 +36,6 @@ echo -e "定时任务启动成功...\n"
 
 if [[ $AutoStartBot == true ]]; then
   echo -e "======================6. 启动bot========================\n"
-  cd $dir_root
   ql bot
 fi
 
@@ -46,6 +44,6 @@ echo -e "容器启动成功..."
 echo -e "\n请先访问5700端口，登录成功面板之后再执行添加定时任务..."
 echo -e "############################################################\n"
 
-crond -f
+crond -f >/dev/null
 
 exec "$@"
--- src/version.ts
+++ src/version.ts
@@ -1,2 +1,2 @@
-export const version = 'v2.2.0-064';
-export const changeLog = 'https://t.me/jiaolongwang/107';
+export const version = 'v2.2.0-066';
+export const changeLog = 'https://t.me/jiaolongwang/109';
